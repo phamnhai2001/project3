8385a17f36fb495003f6a0b6af35b15f3758df32
DESKTOP-V9TNUVN\lan anh <nhaipt@caerux.com>
2022-03-19 15:36:30 +0700

    update coding page login

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,27 @@ setInterval(function() {
   .end()
   .appendTo('#slideshow');
 }, 3000);
+
+// tab content
+$(document).ready(function()
+{
+    function activeTab(obj)
+    {
+        $('.container ul li').removeClass('active');
+ 
+        $(obj).addClass('active');
+ 
+        var id = $(obj).find('a').attr('href');
+ 
+        $('.tab_item').hide();
+ 
+        $(id) .show();
+    }
+ 
+    $('.tab li').click(function(){
+        activeTab(this);
+        return false;
+    });
+ 
+    activeTab($('.tab li:first-child'));
+});
