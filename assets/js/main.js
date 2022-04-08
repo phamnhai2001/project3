0011a9fcf4314e0c4d662defd79377d6b62f4789
phamnhai2001 <phamnhai2001@gmail.com>
2022-04-08 23:29:40 +0700

    uplaod

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,23 @@ $('.slider').slick({
   dots: false,
   autoplay: true,
   infinite: true,
-  cssEase: 'linear'
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
 });
 
 // slider banner
