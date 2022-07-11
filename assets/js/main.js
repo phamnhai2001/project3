dd7b8e3a8552931a95b47edcdaaf461f47cef10e
phamnhai2001 <phamnhai2001@gmail.com>
2022-07-11 13:22:28 +0700

    update banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,17 +23,7 @@ $('.slider').slick({
   ]
 });
 
-// slider banner
-$("#slideshow > .slide:gt(0)").hide();
 
-setInterval(function() { 
-  $('#slideshow > .slide:first')
-  .fadeOut(1000)
-  .next()
-  .fadeIn(1000)
-  .end()
-  .appendTo('#slideshow');
-}, 3000);
 
 // tab content
 $(document).ready(function()
